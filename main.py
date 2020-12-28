fbe683207e896a88a65dfd5c008bafe45fbc5bfe
Matthew Cheney <m.cheney95@outlook.com>
2020-12-28 16:07:04 -0700

    Add site metadata

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Word Translator",
+    description="Translate words into English",
+    version="1.0.0"
+)
 
 import routes  # This is not optional
