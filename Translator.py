5a9918101ec98a4db52f8d5bd93d4a15661099ab
Matthew Cheney <m.cheney95@outlook.com>
2020-12-28 16:20:15 -0700

    Change multiword support

--- Translator.py
+++ Translator.py
@@ -9,21 +9,30 @@ def _LookupInDictionary(lemma: str,
     return DictionaryDAO.getDictionaryEntry(lemma, language_code)
 
 
-def Translate(word: str, language_code: SupportedLanguage) -> [(str, DictionaryEntry)]:
+def Translate(word: str, language_code: SupportedLanguage) -> dict:
     """
     Translate single word. Returns empty list if no translation found.
     :param word: 1 words
     :param language_code: language the word string is in
     :return: list of words and phrase with translations
     """
-    res = []
-    variants = {word, word.lower()}
-    for text, lem in Lemmatizer.lemmatize(word, language_code):
-        variants.add(lem)
-    for text, lem in Lemmatizer.lemmatize(word.lower(), language_code):
-        variants.add(lem)
-    for lem in variants:
-        dbRes = _LookupInDictionary(lem, language_code)
-        if dbRes is not None:
-            res.append({word: dbRes})
+    res = dict()
+    variants = dict()
+    # variants[word] = [word, word.lower()]
+    for w in word.split(' '):
+        variants[w] = [w, w.lower()]
+
+    for text in variants.keys():
+        lemmas = set()
+        for v in variants[text]:
+            lemmas.add(v)
+        meanings = set()
+        for txt, lem in Lemmatizer.lemmatize(text, language_code):
+            lemmas.add(lem)
+        for lem in lemmas:
+            dbRes = _LookupInDictionary(lem, language_code)
+            if dbRes is not None:
+                meanings.add(dbRes)
+        res[text] = meanings
+
     return res
